d87397d582f472044d682e9b9b7e1688e3bf9aa7
motxx <motohiko.ave@gmail.com>
2023-04-15 08:16:47 +0900

    remove

--- nuxt3-app/lit/node/createAccount.lit.js
+++
@@ -1,8 +0,0 @@
-const createAccount = async () => {
-  // check creator permission
-  // add account to subtree
-  // create signature
-  // return signature
-};
-
-createAccount();
--- nuxt3-app/lit/node/getIpInfo.js
+++
@@ -1,7 +0,0 @@
-export async function getIpInfo() {
-  const headers = new Headers();
-  headers.append("User-Agent", "curl/7.72.0");
-  const ipURL = "https:/" + "/ipinfo.io";
-  const r = await fetch(ipURL);
-  return await r.json();
-}
