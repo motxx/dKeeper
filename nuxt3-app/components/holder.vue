a8bb7f5626132de455ae396d3ed4a9aaba19a712
motxx <motohiko.ave@gmail.com>
2023-04-14 19:01:58 +0900

    update

--- nuxt3-app/components/holder.vue
+++ nuxt3-app/components/holder.vue
@@ -7,26 +7,37 @@ const country = ref("Japan");
 const discloseOptions = ["Deny", "Allow"];
 
 const fetchedCredential = ref("");
+const presentation = ref("");
+
+const holder = new Holder();
 
 const fetchCredential = async () => {
-  const res = await new Holder().fetchCredential();
+  const res = await holder.fetchCredential();
   fetchedCredential.value = res;
-}
+};
 
 const createPresentation = async () => {
-  console.log("present");
-}
+  const res = await holder.createPresentation();
+  if (res) {
+    presentation.value = res;
+  }
+};
+
+const execute = async () => {
+  await holder.sendTransaction();
+};
+
 </script>
 
 <template>
   <section class="flex items-center justify-center flex-col h-full max-w-screen-xl mx-auto">
     <div class="w-full px-4">
       <h1 class="text-4xl md:text-5xl font-extrabold text-gray-800 mb-4 md:mb-8 tracking-tight">
-        Holder
+        Authorization
       </h1>
       <!-- Fetch VC -->
       <h2 class="text-2xl md:text-3xl font-extrabold text-gray-800 mb-4 md:mb-8 tracking-tight">
-        Fetch Verifiable Credential
+        Step1. Fetch Verifiable Credential
       </h2>
       <div class="w-full max-w-lg flex flex-col">
         <div class="flex flex-wrap -mx-3 mb-6">
@@ -54,76 +65,99 @@ const createPresentation = async () => {
         </div>
       </div>
       <!-- Create VP -->
-      <h2 class="text-2xl md:text-3xl font-extrabold text-gray-800 mb-4 md:mb-8 tracking-tight">
-        Create Verifiable Presentation
-      </h2>
-      <div class="w-full max-w-lg flex flex-col" v-if="fetchedCredential">
-        <span class="mb-8">Select columns allowed to disclose.</span>
-        <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full px-3 mb-6 md:mb-0">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-username">
-              Name
-            </label>
-            <div class="flex">
-              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-username" type="text" v-model="name" readonly>
-              <div class="relative ml-2 w-40">
-                <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-country">
-                  <option v-for="option in discloseOptions" :value="option" :key="option">{{ option }}</option>
-                </select>
-                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                  <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M17.293 6.293a1 1 0 00-1.414-1.414L10 12.586 6.707 9.293a1 1 0 00-1.414 1.414l4 4a1 1 0 001.414 0l7-7z"/></svg>
+      <div v-if="fetchedCredential">
+        <h2 class="text-2xl md:text-3xl font-extrabold text-gray-800 mb-4 md:mb-8 tracking-tight">
+          Step2. Create Verifiable Presentation
+        </h2>
+        <div class="w-full max-w-lg flex flex-col">
+          <span class="mb-8">Select columns allowed to disclose.</span>
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full px-3 mb-6 md:mb-0">
+              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-username">
+                Name
+              </label>
+              <div class="flex">
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-username" type="text" v-model="name" readonly>
+                <div class="relative ml-2 w-40">
+                  <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-country">
+                    <option v-for="option in discloseOptions" :value="option" :key="option">{{ option }}</option>
+                  </select>
+                  <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M17.293 6.293a1 1 0 00-1.414-1.414L10 12.586 6.707 9.293a1 1 0 00-1.414 1.414l4 4a1 1 0 001.414 0l7-7z"/></svg>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full px-3 mb-6 md:mb-0">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-gender">
-              Gender
-            </label>
-            <div class="flex">
-              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-username" type="text" v-model="gender" readonly>
-              <div class="relative ml-2 w-40">
-                <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-country">
-                  <option v-for="option in discloseOptions" :value="option" :key="option">{{ option }}</option>
-                </select>
-                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                  <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M17.293 6.293a1 1 0 00-1.414-1.414L10 12.586 6.707 9.293a1 1 0 00-1.414 1.414l4 4a1 1 0 001.414 0l7-7z"/></svg>
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full px-3 mb-6 md:mb-0">
+              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-gender">
+                Gender
+              </label>
+              <div class="flex">
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-username" type="text" v-model="gender" readonly>
+                <div class="relative ml-2 w-40">
+                  <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-country">
+                    <option v-for="option in discloseOptions" :value="option" :key="option">{{ option }}</option>
+                  </select>
+                  <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M17.293 6.293a1 1 0 00-1.414-1.414L10 12.586 6.707 9.293a1 1 0 00-1.414 1.414l4 4a1 1 0 001.414 0l7-7z"/></svg>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div class="flex flex-wrap -mx-3 mb-6">
-          <div class="w-full px-3 mb-6 md:mb-0">
-            <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-country">
-              Country
-            </label>
-            <div class="flex">
-              <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-username" type="text" v-model="country" readonly>
-              <div class="relative ml-2 w-40">
-                <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-country">
-                  <option v-for="option in discloseOptions" :value="option" :key="option">{{ option }}</option>
-                </select>
-                <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                  <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M17.293 6.293a1 1 0 00-1.414-1.414L10 12.586 6.707 9.293a1 1 0 00-1.414 1.414l4 4a1 1 0 001.414 0l7-7z"/></svg>
+          <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full px-3 mb-6 md:mb-0">
+              <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-country">
+                Country
+              </label>
+              <div class="flex">
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-username" type="text" v-model="country" readonly>
+                <div class="relative ml-2 w-40">
+                  <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-country">
+                    <option v-for="option in discloseOptions" :value="option" :key="option">{{ option }}</option>
+                  </select>
+                  <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M17.293 6.293a1 1 0 00-1.414-1.414L10 12.586 6.707 9.293a1 1 0 00-1.414 1.414l4 4a1 1 0 001.414 0l7-7z"/></svg>
+                  </div>
                 </div>
               </div>
+            </div>
+          </div>
+
+          <div class="flex flex-wrap -mx-3 mb-6 mt-auto">
+            <div class="w-full px-3">
+              <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="createPresentation">
+                Create Presentation
+              </button>
             </div>
           </div>
         </div>
 
-        <div class="flex flex-wrap -mx-3 mb-6 mt-auto">
-          <div class="w-full px-3">
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="createPresentation">
-              Create Presentation
-            </button>
+        <!-- Execute -->
+        <div v-if="presentation.length > 0">
+          <h2 class="text-2xl md:text-3xl font-extrabold text-gray-800 mb-4 md:mb-8 tracking-tight">
+            Step3. Execute dApp
+          </h2>
+          <div class="w-full max-w-lg flex flex-col">
+            <div class="flex flex-wrap -mx-3 mb-6">
+              <div class="w-full px-3 mb-6 md:mb-0">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-contract">
+                  Contract Address
+                </label>
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-contract" type="text" placeholder="Enter contract address">
+              </div>
+            </div>
+            <div class="flex flex-wrap -mx-3 mb-6 mt-auto">
+              <div class="w-full px-3">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="execute">
+                  Execute
+                </button>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="w-full max-w-lg flex flex-col" v-else>
-        Fetch credential first.
       </div>
     </div>
   </section>
--- nuxt3-app/usecase/holder.ts
+++ nuxt3-app/usecase/holder.ts
@@ -1,7 +1,26 @@
 import signedDocument from "../zk/data/signedDocument.json";
 
 export class Holder {
+  credentials: string | null = null;
+  presentation: string | null = null;
+
+  constructor() {
+  }
+
   fetchCredential = async () => {
-    return JSON.stringify(signedDocument);
+    this.credentials = JSON.stringify(signedDocument);
+    return this.credentials;
+  };
+
+  createPresentation = async () => {
+    if (!this.credentials) {
+      return null;
+    }
+    this.presentation = "present";
+    return this.presentation;
+  };
+
+  sendTransaction = async () => {
+    console.log("execute dApp");
   };
 }
