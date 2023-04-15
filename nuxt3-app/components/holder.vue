650d099814e2513187850ae6d6366feec7722df7
motxx <motohiko.ave@gmail.com>
2023-04-15 21:10:08 +0900

    save

--- nuxt3-app/components/holder.vue
+++ nuxt3-app/components/holder.vue
@@ -69,8 +69,10 @@ const execute = async () => {
         <h2 class="text-2xl md:text-3xl font-extrabold text-gray-800 mb-4 md:mb-8 tracking-tight">
           Step2. Create Verifiable Presentation
         </h2>
+
+        <!-- VC ACL -->
         <div class="w-full max-w-lg flex flex-col">
-          <span class="mb-8">Select columns allowed to disclose.</span>
+          <span class="mb-8">1. Select columns allowed to disclose.</span>
           <div class="flex flex-wrap -mx-3 mb-6">
             <div class="w-full px-3 mb-6 md:mb-0">
               <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-username">
@@ -126,6 +128,20 @@ const execute = async () => {
             </div>
           </div>
 
+          <!-- Real-time ACL -->
+          <div class="w-full max-w-lg flex flex-col">
+            <span class="mb-8">2. Add real-time verification columns.</span>
+            <div class="flex flex-wrap -mx-3 mb-6">
+              <div class="w-full px-3 mb-6 md:mb-0">
+                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-youtube-video-id">
+                  YouTube Video Id
+                </label>
+                <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-youtube-video-id" type="text" placeholder="Enter YouTube Video Id">
+              </div>
+            </div>
+          </div>
+
+          <!-- VP Button -->
           <div class="flex flex-wrap -mx-3 mb-6 mt-auto">
             <div class="w-full px-3">
               <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" @click="createPresentation">
@@ -134,6 +150,7 @@ const execute = async () => {
             </div>
           </div>
 
+          <!-- Derived Proof -->
           <div class="flex flex-wrap -mx-3 mb-6" v-if="presentation">
             <div class="w-full px-3 mb-6 md:mb-0">
               <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-presentation">
@@ -142,6 +159,7 @@ const execute = async () => {
               <textarea v-model="presentation" id="grid-presentation" class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" readonly></textarea>
             </div>
           </div>
+
         </div>
 
         <!-- Execute -->
